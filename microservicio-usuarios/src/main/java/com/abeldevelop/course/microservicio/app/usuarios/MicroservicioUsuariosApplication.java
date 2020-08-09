402a4a402dcc904e3bf1c7a2d24bf8119928d148
Abel <abeldevelop@users.noreply.github.com>
2020-08-09 15:17:27 +0200

    Conectamos microservicio usuarios con eureka

--- microservicio-usuarios/src/main/java/com/abeldevelop/course/microservicio/app/usuarios/MicroservicioUsuariosApplication.java
+++ microservicio-usuarios/src/main/java/com/abeldevelop/course/microservicio/app/usuarios/MicroservicioUsuariosApplication.java
@@ -2,12 +2,13 @@ package com.abeldevelop.course.microservicio.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroservicioUsuariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicioUsuariosApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(MicroservicioUsuariosApplication.class, args);
+  }
 }

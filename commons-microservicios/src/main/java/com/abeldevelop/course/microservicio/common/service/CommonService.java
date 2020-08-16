03386d73df0b264766b5bca7fddb8fedcdc54886
Abel <abeldevelop@users.noreply.github.com>
2020-08-16 19:57:57 +0200

    Backend: Paginación

--- commons-microservicios/src/main/java/com/abeldevelop/course/microservicio/common/service/CommonService.java
+++ commons-microservicios/src/main/java/com/abeldevelop/course/microservicio/common/service/CommonService.java
@@ -3,11 +3,18 @@ package com.abeldevelop.course.microservicio.common.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CommonService<E> {
-    
-    List<E> findAll();
-    Optional<E> findById(Long id);
-    E save(E alumno);
-    void deleteById(Long id);
-    
+
+  Page<E> findAll(Pageable pageable);
+
+  List<E> findAll();
+
+  Optional<E> findById(Long id);
+
+  E save(E alumno);
+
+  void deleteById(Long id);
 }
--- commons-microservicios/src/main/java/com/abeldevelop/course/microservicio/common/service/impl/CommonServiceImpl.java
+++ commons-microservicios/src/main/java/com/abeldevelop/course/microservicio/common/service/impl/CommonServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +14,12 @@ import com.abeldevelop.course.microservicio.common.service.CommonService;
 public class CommonServiceImpl<E, R extends JpaRepository<E, Long>> implements CommonService<E> {
 
   @Autowired protected R repository;
+
+  @Transactional(readOnly = true)
+  @Override
+  public Page<E> findAll(Pageable pageable) {
+    return repository.findAll(pageable);
+  }
 
   @Transactional(readOnly = true)
   @Override
